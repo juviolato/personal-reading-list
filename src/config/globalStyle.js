d085e75dc3b42d363db4d5696c2b1ad35f567a6f
Julia Violato <5183043-juviolato@users.noreply.gitlab.com>
2020-04-18 16:47:02 -0300

    fixed css

--- src/config/globalStyle.js
+++ src/config/globalStyle.js
@@ -3,6 +3,4 @@ import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
 	${reset}
-	
-	@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap');
 `;
